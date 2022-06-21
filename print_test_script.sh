dd733ac639d4c7db9f976dc10b441aebf21d34fa
Philippe Parisot <pparisot3@gmail.com>
2022-06-21 12:04:12 -0400

    Updated script, clean output

--- print_test_script.sh
+++ print_test_script.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 set -x
 
-cargo run -- create-account Phil 5
-cargo run -- create-account Rizza 1000
-cargo run -- balance Phil
-cargo run -- balance Rizza
-cargo run -- transfer Phil Rizza 5
-cargo run -- balance Phil
-cargo run -- balance Rizza
+cargo run -q -- create-account Phil 5
+cargo run -q -- create-account Rizza 1000
+cargo run -q -- balance Phil
+cargo run -q -- balance Rizza
+cargo run -q -- transfer Phil Rizza 5
+cargo run -q -- balance Phil
+cargo run -q -- balance Rizza
 
 sudo rm -f prod_data
